e537c7aa9af63f1a8c35f191b992c63054eaf8ee
Evgeny Rybin <z.ribin20@gmail.com>
2023-01-04 18:55:51 +0500

    Disable JavaScript

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ app.use(express.json());
 
 app.post("/", async (req, res) => {
   const { selector, url } = req.body;
+  console.log(`URL: ${url} WITH SELECTOR: ${selector}`)
   const browser = await puppeteer.launch({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
+  await page.setRequestInterception(true);
+  page.on("request", (request) => {
+    if (request.resourceType() === "script") request.abort();
+    else request.continue();
+  });
   await page.goto(url);
 
   await page.waitForSelector(selector);
